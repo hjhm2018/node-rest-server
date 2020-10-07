4c5491493b1157886218be63be0818658567611e
hjhm2018 <henryjherreram@gmail.com>
2020-10-07 12:18:47 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 } else{
-    urlDB = 'mongodb+srv://henry:gEUU2ZNd3qifoNY2@cluster0.m8lwo.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
